68618ebdc78645a7481803cff11d26cb37ae2ce2
lethang0922 <le.thang0922@gmail.com>
2023-03-30 19:18:30 -0700

    Update CS380_lab0.java

--- CS380_lab0/CS380_lab0.java
+++ CS380_lab0/CS380_lab0.java
@@ -3,5 +3,18 @@ package pachagetest;
 public class CS380_lab0 {
 		public static void main ( String[] args) {
 			System.out.println("First Lab CS 380");
+			int a=reverse(123);
+			System.out.println(a);
+		}
+		public static int reverse(int n) {
+			int reverse = 0;
+		 
+		      while( n != 0 )
+		      {
+		          reverse = reverse * 10;
+		          reverse = reverse + n%10;
+		          n = n/10;
+		      }
+		      return reverse;
 		}
 }
